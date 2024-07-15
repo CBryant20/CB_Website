d2aa9d8e23e64fc8f1b0bd0b3d25c68e0f45fe6f
CBryant20 <150076515+CBryant20@users.noreply.github.com>
2024-07-15 10:59:01 -0700

    Mobile Update 3

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useLocation } from "react-router-dom";
 
 import AppHero from "../home/Hero";
@@ -9,6 +9,7 @@ import AppContact from "../home/Contact";
 export default function AppHome() {
   const [selectedProject, setSelectedProject] = useState(null);
   const location = useLocation();
+  const blockRefs = useRef([]);
 
   useEffect(() => {
     if (location.state?.target) {
@@ -17,6 +18,31 @@ export default function AppHome() {
         .scrollIntoView({ behavior: "smooth" });
     }
   }, [location.state]);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.3,
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    blockRefs.current.forEach((ref) => {
+      observer.observe(ref);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   const handleProjectSelect = (project) => {
     setSelectedProject(project);
@@ -28,16 +54,16 @@ export default function AppHome() {
         selectedProject
       ) : (
         <>
-          <div className='block'>
+          <div className='block' ref={(el) => (blockRefs.current[0] = el)}>
             <AppHero />
           </div>
-          <div className='block'>
+          <div className='block' ref={(el) => (blockRefs.current[1] = el)}>
             <AppAbout />
           </div>
-          <div className='block'>
+          <div className='block' ref={(el) => (blockRefs.current[2] = el)}>
             <AppWork onSelectProject={handleProjectSelect} />
           </div>
-          <div className='block'>
+          <div className='block' ref={(el) => (blockRefs.current[3] = el)}>
             <AppContact />
           </div>
         </>
